154a367450db77d0ce9fcc698885447181a2c844
Dimas <Dimas@DESKTOP-FGTEKP2>
2022-10-15 09:17:01 +0700

    Добавьте файлы проекта.

---
+++ Лаб1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template 
+int x, y;
+Console.WriteLine("Введите x");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y");
+y = Convert.ToInt32(Console.ReadLine());
+
+int z = x;
+x = y;
+y = z;
+
+Console.WriteLine($"x = {x}, y = {y}");
+
+Console.ReadKey();
